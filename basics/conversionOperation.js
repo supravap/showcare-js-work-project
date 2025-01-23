de341f0cf18ee461af06b0075670bd617091f18c
Suprava Priyadarshini <supravap@users.noreply.github.com>
2025-01-23 23:13:51 +0000

    conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "37abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "37" => 33
+//"33abc" => NaN
+//true => 1
+
+let isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 0 => flase
+// "" => false
+// undefined => false
+// "suprava" => true
+// null => false
